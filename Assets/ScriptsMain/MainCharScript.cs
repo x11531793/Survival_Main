2a607a68ab45d9e0d7eca28ba06ce00d3144518a
x11531793 <x11531793@student.ncirl.ie>
2017-02-22 14:58:58 +0000

    Survival

---
+++ Assets/ScriptsMain/MainCharScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharScript : MonoBehaviour {
+
+    public Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+
+        myAnimator = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+		
+	}
+}
---
+++ Assets/ScriptsMenu/MenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas QuitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        QuitMenu = QuitMenu.GetComponent<Canvas>();
+        startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        QuitMenu.enabled = false;
+	}
+
+    public void ExitPress()
+    {
+        QuitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void noPress()
+    {
+        QuitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartGame ()
+    {
+        SceneManager.LoadScene("enviro");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
